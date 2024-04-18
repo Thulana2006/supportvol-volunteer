212168a65857e5380a770e3546da3498f0c26130
Thulana2006 <thulanabaji@gmail.com>
2024-04-19 04:35:44 +0530

    page updated for the required structure

--- app/community/page.tsx
+++ app/community/page.tsx
@@ -1,25 +1,13 @@
 "use client";
 
 import React, { useEffect, useState } from 'react';
-import {
-    Accordion,
-    AccordionContent,
-    AccordionItem,
-    AccordionTrigger,
-  } from "@/components/ui/accordion";
 
 import { UserAuth } from "../../context/AuthContext";
 import {Loader2} from 'lucide-react';
-import { Button } from '@/components/ui/button';
-import AuthenticationForm from '@/components/landing/authenticationForm';
 import Navbar from '@/components/community/navbar';
 import CommunityCarousel from '@/components/community/communitiesCarousel';
 
-export const Icons = {
-  spinner: Loader2,
-};
-
-const Page: React.FC = () => {
+export default function CommunityPage() {
     const { user } = UserAuth();
     const [loading, setLoading] = useState(true);
 
@@ -36,7 +24,7 @@ const Page: React.FC = () => {
     return (
         <div className="flex min-h-screen flex-col gap-6 lg:px-24">
             {loading ? (
-        <Icons.spinner className="h-4 w-4 animate-spin" />
+        <Loader2 className="h-4 w-4 animate-spin" />
       ) : user ? (
         <div className="flex min-h-screen flex-col gap-6 md:px-24 pt-8 ">
           <Navbar selectedItem='Community' userName={user.displayName} userEmail={user.email} userProfile={user.photoURL} selectedIcon={1}/>
@@ -54,5 +42,3 @@ const Page: React.FC = () => {
         </div>
     );
 };
-
-export default Page;
